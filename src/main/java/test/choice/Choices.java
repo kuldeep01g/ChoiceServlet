4f9cfc8149e88d7d3891399e5970d39f757a5afa
TL <TL@gmail.com>
2025-02-13 13:13:47 +0530

    choice.java commit

---
+++ src/main/java/test/choice/Choices.java
@@ -0,0 +1,28 @@
+package test.choice;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+@WebServlet("/dis")
+
+public class Choices extends GenericServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException 
+	{
+		String sb=req.getParameter("sb");
+		
+		if(sb.equals("Add")) {
+			RequestDispatcher rd=req.getRequestDispatcher("ad");
+			rd.forward(req, res);
+		}else if(sb.equals("Avg")) {
+			RequestDispatcher rd=req.getRequestDispatcher("av");
+			rd.forward(req, res);
+		}else{
+			RequestDispatcher  rd=req.getRequestDispatcher("ml");
+			rd.forward(req, res);
+			
+		}
+		
+	}
+
+}
