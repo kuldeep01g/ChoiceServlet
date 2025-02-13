54329268198b7565f59502e67d7489dd637aa047
TL <TL@gmail.com>
2025-02-13 13:53:21 +0530

    multiplication.java commit

---
+++ src/main/java/test/choice/Multiplication.java
@@ -0,0 +1,24 @@
+package test.choice;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+@SuppressWarnings("serial")
+@WebServlet("/ml")
+public class Multiplication extends GenericServlet {
+	
+	public void service(ServletRequest req,ServletResponse res)throws ServletException,IOException
+	{
+		int x=Integer.parseInt(req.getParameter("v1"));
+		int y=Integer.parseInt(req.getParameter("v2"));
+		int z=x*y;
+		
+		PrintWriter pw=res.getWriter();
+		res.setContentType("text/html");
+		pw.println("The Multiplication is :"+z+"<br>");
+		
+		RequestDispatcher rd=req.getRequestDispatcher("input.html");
+		rd.include(req, res);
+		
+	}
+
+}
